68836516c347bcc0c4436c9987d3268d561f29e5
pjwomble <32985946+pjwomble@users.noreply.github.com>
2019-09-20 10:58:54 -0700

    Update azuredeploy.sh

--- slurm/azuredeploy.sh
+++ slurm/azuredeploy.sh
@@ -92,22 +92,35 @@ sudo chown $ADMIN_USERNAME $mungekey
 echo "Start looping all workers" >> /tmp/azuredeploy.log.$$ 2>&1 
 
 i=0
+sudo touch /tmp/error.txt
+sudo touch /tmp/error2.txt
+
 while [ $i -lt $NUM_OF_VM ]
 do
    worker=$WORKER_NAME$i
 
    echo "SCP to $worker"  >> /tmp/azuredeploy.log.$$ 2>&1 
-   sudo rm /tmp/error.txt
-   sudo touch /tmp/error.txt
-   sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/error.txt.$$ 2>&1
+   sudo true > /tmp/error.txt
+
+   sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/error.txt 2>&1
+   sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/error.txt 2>&1
+   sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/error.txt 2>&1
    
    while (( $(grep -c "Permission denied" /tmp/error.txt) >= 1 ))
    do  
-    echo "$worker"
-    sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/error.txt.$$ 2>&1
-    sleep 1
-    echo "waiting for worker to come online" >> /tmp/azuredeploy.log.$$ 2>&1
-  done  
+     echo "$worker" >> /tmp/azuredeploy.log.$$ 2>&1
+     
+     sudo true > /tmp/error2.txt
+  
+     sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/error2.txt 2>&1
+     sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/error2.txt 2>&1
+     sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/error2.txt 2>&1
+     
+     sudo cp -r /tmp/error2.txt /tmp/error.txt
+     
+     sleep 1
+     echo "waiting for worker to come online" >> /tmp/azuredeploy.log.$$ 2>&1
+   done  
     
    sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/azuredeploy.log.$$ 2>&1 
    sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
