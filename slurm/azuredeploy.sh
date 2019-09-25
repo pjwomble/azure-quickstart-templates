b7a2e28016d5e75504dc617e721e7b759a564862
pjwomble <32985946+pjwomble@users.noreply.github.com>
2019-09-24 18:00:55 -0700

    Update azuredeploy.sh

--- slurm/azuredeploy.sh
+++ slurm/azuredeploy.sh
@@ -110,7 +110,6 @@ do
    sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/error.txt 2>&1
    sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/error.txt 2>&1
    sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/error.txt 2>&1
-   echo $WORKER_NAME$i > $ADMIN_USERNAME@$worker:/etc/hostname
    
    echo "look here" >> /tmp/azuredeploy.log.$$ 2>&1
    grep -c "Permission denied" /tmp/error.txt >> /tmp/azuredeploy.log.$$ 2>&1
@@ -125,7 +124,6 @@ do
       sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/error2.txt 2>&1
       sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/error2.txt 2>&1
       sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/error2.txt 2>&1
-      echo $WORKER_NAME$i > $ADMIN_USERNAME@$worker:/etc/hostname
      
       echo "error2txt" >> /tmp/azuredeploy.log.$$ 2>&1
       cat /tmp/error2.txt >> /tmp/azuredeploy.log.$$ 2>&1
@@ -139,7 +137,6 @@ do
    sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key >> /tmp/azuredeploy.log.$$ 2>&1 
    sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf >> /tmp/azuredeploy.log.$$ 2>&1
    sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts >> /tmp/azuredeploy.log.$$ 2>&1
-   echo $WORKER_NAME$i > $ADMIN_USERNAME@$worker:/etc/hostname
 
    echo "Remote execute on $worker" >> /tmp/azuredeploy.log.$$ 2>&1 
    sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker >> /tmp/azuredeploy.log.$$ 2>&1 << 'ENDSSH1'
